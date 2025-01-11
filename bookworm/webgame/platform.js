b823116e3179ffba18a6d6cd7324ba3a8f68eb11
WarRobotDoge <85980136+WarRobotDoge@users.noreply.github.com>
2025-01-11 13:42:06 -0800

    Create platform.js

---
+++ bookworm/webgame/platform.js
@@ -0,0 +1,30 @@
+//use $.platform.<platformname> to return true/false
+var userOSAgent = navigator.userAgent.toLowerCase();
+// Figure out what browser is being used
+jQuery.platform = {
+        mac: /mac/.test(userOSAgent),
+        osx: /mac os x/.test(userOSAgent),
+        win: /win/.test(userOSAgent),
+        linux: /linux/.test(userOSAgent)
+};
+/*
+     FILE ARCHIVED ON 01:40:02 Jul 24, 2009 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 17:54:52 Sep 08, 2018.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  LoadShardBlock: 94.38 (3)
+  esindex: 0.007
+  captures_list: 112.375
+  CDXLines.iter: 13.803 (3)
+  PetaboxLoader3.datanode: 73.803 (4)
+  exclusion.robots: 0.133
+  exclusion.robots.policy: 0.123
+  RedisCDXSource: 1.589
+  PetaboxLoader3.resolve: 58.823 (2)
+  load_resource: 40.412
+*/
